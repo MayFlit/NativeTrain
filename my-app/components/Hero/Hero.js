b984e2b7c46a119f70387615f1a28fa959933849
MayFlit <MayFlit@gmail.com>
2022-05-02 13:34:34 +0300

    Save

--- my-app/components/Hero/Hero.js
+++ my-app/components/Hero/Hero.js
@@ -2,22 +2,12 @@ import React from "react";
 import {View, Image} from "react-native";
 import {HeroStyle} from "./style";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {useDispatch} from "react-redux";
 
 
 export const Hero = () => {
-
-
-
-    async function lala() {
-        // await AsyncStorage.setItem(
-        //     'lalailaKey',
-        //     'lalailaValue'
-        // );
-        const lala = await AsyncStorage.getItem('lalailaKey')
-        console.log(lala)
-    }
-    lala()
-
+    const dispatch = useDispatch()
+    const
 
 
     return (
