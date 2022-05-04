cfbca1e21de41cde7e58838a1a874b0062b7a2f8
MayFlit <MayFlit@gmail.com>
2022-05-04 12:15:48 +0300

    Problem solving

--- my-app/redux/reducers/rootReducer.js
+++ my-app/redux/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { heroReducer } from "./heroReducer";
+import {enemyReducer} from "./enemyReducer";
 
 export const rootReducer = combineReducers({
     hero: heroReducer,
+    enemy: enemyReducer,
 })
