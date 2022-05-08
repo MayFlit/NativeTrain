d107c4b1998463418380e7f34d80bd0f718a0e46
Daria <dariaaiv@gmail.com>
2022-05-08 20:13:11 +0300

    ProfileAndSkills

---
+++ my-app/components/Skills/Skills.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {SafeAreaView, ImageBackground, Text, Image, TouchableOpacity } from 'react-native';
+import {Hero} from "../Hero/Hero";
+import {Enemy} from "../Enemy/Enemy";
+import hero from '../../store/hero';
+import enemy from '../../store/enemy';
+import {observer} from "mobx-react-lite";
+import {SkillsStyle} from "./style";
+
+
+export const Skills = observer (() => {
+  const loadScene = () => {
+    navigation.navigate('Shop');
+  }
+
+    return (
+            <SafeAreaView style={SkillsStyle.box}>
+
+              <TouchableOpacity onPress={loadScene}>
+                <Text style={SkillsStyle.container}><Image source={require('../../assets/skills/health.png')} style={SkillsStyle.icons}/> </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={loadScene}>
+                <Text style={SkillsStyle.container}><Image source={require('../../assets/skills/blood.png')} style={SkillsStyle.icons}/> </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={loadScene}>
+                <Text style={SkillsStyle.container}> <Image source={require('../../assets/skills/damage.png')} style={SkillsStyle.icons} /> </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={loadScene}>
+                <Text style={SkillsStyle.container}><Image source={require('../../assets/skills/x2.png')} style={SkillsStyle.icons}/></Text>
+                </TouchableOpacity>
+
+            </SafeAreaView>
+    );
+})
