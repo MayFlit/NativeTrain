956461db2705b5f76ec37332e66c6ef0647ba41d
Daria <dariaaiv@gmail.com>
2022-05-13 02:33:45 +0300

    MainPage

--- my-app/components/MainMenu/style.js
+++ my-app/components/MainMenu/style.js
@@ -5,10 +5,79 @@ export const MainMenuStyle = StyleSheet.create({
     flex: 1,
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    backgroundColor: '#DEE9FD'
   },
-  text: {
-    fontSize: 50,
-    color: 'red',
+  topContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  inner: {
+    backgroundColor: '#DEE9F7',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#E2ECFD',
+    borderWidth: 1
+  },
+  topShadow: {
+    shadowOffset: {
+      width: -6,
+      height: -6
+    },
+    shadowOpacity: 1,
+    shadowRadius: 6,
+    shadowColor: "#FBFFFF"
+  },
+  bottomShadow: {
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 6,
+    shadowColor: "#B7C4DD"
+  },
+  playing: {
+    color: "#91A1BD",
+    fontWeight: "800"
+  },
+  artContainer: {
+    marginVertical: 32,
+    alignItems: "center"
+  },
+  avaArt: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderColor: '#D7E1F3',
+    borderWidth: 10
+  },
+  avaContainer: {
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 30,
+    color: '#6C7A93',
+    fontWeight: '600'
+  },
+  artist: {
+    fontSize: 14,
+    marginTop: 6,
+    color: "#91A1BD",
+    fontWeight: '500'
+  },
+  trackContainer: {
+    marginTop: 32,
+    marginBottom: 64
+  },
+  time: {
+    fontSize: 10,
+    color: "#91A1BD",
+    fontWeight: '700'
+  },
+  controlsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around"
   }
 })
